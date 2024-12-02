ebe83e0277845489922d0d72288f76803279a5a3
alvarogomez05 <alvatox11@gmail.com>
2024-12-02 21:19:18 +0100

    slider y productos destacados

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,27 @@
  menubtn.addEventListener('click', () => {
    mobilemenu.classList.toggle('hidden');
  });
+
+    // Script para la funcionalidad de desplazamiento del carrusel
+    let currentIndex = 0;
+    const images = document.querySelectorAll('#carousel > div');
+    const totalImages = images.length;
+
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    const carousel = document.getElementById('carousel');
+
+    function updateCarousel() {
+      const offset = -currentIndex * 100;
+      carousel.style.transform = `translateX(${offset}%)`;
+    }
+
+    prevButton.addEventListener('click', () => {
+      currentIndex = (currentIndex === 0) ? totalImages - 1 : currentIndex - 1;
+      updateCarousel();
+    });
+
+    nextButton.addEventListener('click', () => {
+      currentIndex = (currentIndex === totalImages - 1) ? 0 : currentIndex + 1;
+      updateCarousel();
+    });
